6a599183872bf9b3453e469c1e2a8d4b418720fe
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-08-25 14:21:25 +0530

    serverCreated

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-Janiha edit
+const port = process.env.PORT || 5000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken")
+const multer = require ("multer");
+const path = require("path")
+const cors = require ("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Databaseconnection
+mongoose.connect("mongodb+srv://testsalon:dami1234@testsalon.uwzfg.mongodb.net/SalonPabaluITP");
+
+app.listen(port,(error) =>{
+    if(!error){
+        console.log("Server Running on port" , port)
+
+    }else{
+        console.log("error:" + error)
+    }
+})
